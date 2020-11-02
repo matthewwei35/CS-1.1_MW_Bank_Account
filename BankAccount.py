c5f54f06e854d7643f82e680f7dbb4d7aa9a2e66
Matthew Wei <matthewwei35@gmail.com>
2020-11-01 22:23:25 -0800

    Changed account_number

--- BankAccount.py
+++ BankAccount.py
@@ -1,6 +1,10 @@
+import random
+
 class BankAccount:
+    # Class Attributes
     routing_number = 98765432
-    def __init__(self, full_name, account_number):
+    def __init__(self, full_name):
+        # Instance Attributes
         self.full_name = full_name
-        self.account_number = account_number
+        self.account_number = random.randint(10000000, 99999999)
         self.balance = 0
